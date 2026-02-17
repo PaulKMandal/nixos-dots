ab857c35012eb47389cd5b1851ae5b70ea675671
PaulKMandal <pkmandal221@gmail.com>
2026-02-17 16:05:55 -0600

    Removed waterfox from outputs in flake

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       home-manager.inputs.nixpkgs.follows = "nixpkgs";
    };
 
-   outputs = { self, nixpkgs, home-manager, waterfox, ...}:
+   outputs = { self, nixpkgs, home-manager, ...}:
    let
       system = "x86_64-linux";
       hostname = "nixos";
