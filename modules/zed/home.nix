acef5c5cdb82ac29c462f7d0b938e4b967957716
PaulKMandal <pkmandal221@gmail.com>
2026-02-17 20:45:37 -0600

    Updated format to use safer syntax

--- modules/zed/home.nix
+++ modules/zed/home.nix
@@ -1,25 +1,33 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  xdg.configFile."zed/settings.json".text = builtins.toJSON {
-    icon_theme = {
-      mode = "system";
-      light = "Zed (Default)";
-      dark = "Zed (Default)";
+  programs.zed-editor = {
+    enable = true;
+
+    # IMPORTANT: allow Zed to write/migrate settings.json
+    mutableUserSettings = true;
+
+    userSettings = {
+      icon_theme = {
+        mode = "system";
+        light = "Zed (Default)";
+        dark = "Zed (Default)";
+      };
+
+      ui_font_size = 16;
+      buffer_font_size = 15;
+
+      theme = {
+        mode = "system";
+        light = "One Light";
+        dark = "One Dark";
+      };
+
+      format_on_save = true;
     };
 
-    ui_font_size = 16;
-    buffer_font_size = 15;
-
-    theme = {
-      mode = "system";
-      light = "One Light";
-      dark = "One Dark";
-    };
-
-    format_on_save = true;
+    # optional later:
+    # extensions = [ "nix" "rust" ];
+    # extraPackages = [ pkgs.nixd pkgs.rust-analyzer ];
   };
-
-  # If you later want:
-  # xdg.configFile."zed/keymap.json".text = builtins.toJSON [ ... ];
 }
 
