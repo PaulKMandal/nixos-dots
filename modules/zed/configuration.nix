e57de118aa701a23e83798af703713c2b51e7cf3
PaulKMandal <pkmandal221@gmail.com>
2026-02-17 21:24:19 -0600

    Added autocomplete for C/C++

--- modules/zed/configuration.nix
+++ modules/zed/configuration.nix
@@ -3,6 +3,13 @@
   environment.systemPackages = with pkgs; [
     zed-editor
 
+    # C/C++
+    clang-tools        # provides clangd (LSP), clang-format, etc.
+    bear               # optional: generate compile_commands.json from make
+    cmake              # optional but common
+    gnumake            # optional but common
+    pkg-config         # optional but common
+
     # Nix
     nixd
     alejandra
