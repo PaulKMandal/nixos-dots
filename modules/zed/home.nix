ea04633cbb0b7f961f02a08b5a4fd6288af57d75
PaulKMandal <pkmandal221@gmail.com>
2026-02-17 21:39:53 -0600

    Added autocomplete to zed config

--- modules/zed/home.nix
+++ modules/zed/home.nix
@@ -23,11 +23,30 @@
       };
 
       format_on_save = "on";
+
+      show_completions_on_input = true;
     };
 
     # optional later:
     # extensions = [ "nix" "rust" ];
     # extraPackages = [ pkgs.nixd pkgs.rust-analyzer ];
+    extraPackages = with pkgs; [
+      # Nix
+      nixd
+
+      # Rust
+      rust-analyzer
+
+      # Python
+      pyright
+
+      # C/C++
+      clang-tools  # provides clangd
+
+      # TS/JS (if you want it)
+      nodejs_22
+      nodePackages.typescript-language-server
+    ];
   };
 }
 
