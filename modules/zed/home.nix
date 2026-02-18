4bda2714a763e09f277384f935682c5a5517fd01
PaulKMandal <pkmandal221@gmail.com>
2026-02-17 22:49:44 -0600

    restored home.nix, absolute path not necessary for lsp

--- modules/zed/home.nix
+++ modules/zed/home.nix
@@ -25,62 +25,28 @@
       format_on_save = "on";
 
       show_completions_on_input = true;
-
-      # Tell Zed which language server to prefer for each language
-      languages = {
-        Python = { language_servers = [ "pyright" "..." ]; };
-        Nix = { language_servers = [ "nixd" "..." ]; };
-        Rust = { language_servers = [ "rust-analyzer" "..." ]; };
-    
-        C = { language_servers = [ "clangd" "..." ]; };
-        "C++" = { language_servers = [ "clangd" "..." ]; };
-    
-        JavaScript = { language_servers = [ "typescript-language-server" "..." ]; };
-        TypeScript = { language_servers = [ "typescript-language-server" "..." ]; };
-        TSX = { language_servers = [ "typescript-language-server" "..." ]; };
-      };
-    
-      # Force Zed to use the system binaries (critical on NixOS)
-      lsp = {
-        pyright = {
-          binary = {
-            ignore_system_version = false;
-            path = "/run/current-system/sw/bin/pyright-langserver";
-            arguments = [ "--stdio" ];
-          };
-        };
-    
-        nixd = {
-          binary = {
-            ignore_system_version = false;
-            path = "/run/current-system/sw/bin/nixd";
-          };
-        };
-    
-        "rust-analyzer" = {
-          binary = {
-            ignore_system_version = false;
-            path = "/run/current-system/sw/bin/rust-analyzer";
-          };
-        };
-    
-        clangd = {
-          binary = {
-            ignore_system_version = false;
-            path = "/run/current-system/sw/bin/clangd";
-          };
-        };
-    
-        "typescript-language-server" = {
-          binary = {
-            ignore_system_version = false;
-            path = "/run/current-system/sw/bin/typescript-language-server";
-            arguments = [ "--stdio" ];
-          };
-        };
-      };
     };
 
+    # optional later:
+    # extensions = [ "nix" "rust" ];
+    # extraPackages = [ pkgs.nixd pkgs.rust-analyzer ];
+    extraPackages = with pkgs; [
+      # Nix
+      nixd
+
+      # Rust
+      rust-analyzer
+
+      # Python
+      pyright
+
+      # C/C++
+      clang-tools  # provides clangd
+
+      # TS/JS (if you want it)
+      nodejs_22
+      nodePackages.typescript-language-server
+    ];
   };
 }
 
